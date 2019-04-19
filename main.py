3e871ebccf6c7b24ffd64e325788dd906926bedd
15222617310 <15222617310@163.com>
2019-04-19 09:28:37 +0800

    定时执行

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # coding=utf-8
 from scrapy import cmdline
+from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
 
-cmdline.execute("scrapy crawl toutiao".split())
+
+def job():
+    cmdline.execute("scrapy crawl toutiao".split())
+
+
+if __name__ == '__main__':
+    sched = BlockingScheduler()
+    sched.add_job(job, trigger='interval', seconds=60 * 30,
+                  next_run_time=datetime.datetime.now())
