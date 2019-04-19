5e6b0e4e386aee8972de1f004f8a7e15f589884d
15222617310 <15222617310@163.com>
2019-04-19 10:07:36 +0800

    定时30分钟爬取一次

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 # coding=utf-8
+import datetime
 from scrapy import cmdline
+from scrapy.crawler import CrawlerProcess
+from spider.spiders.toutiao_spider import XuanchuanbuSpider
+from apscheduler.schedulers.blocking import BlockingScheduler
+import multiprocessing
 
-cmdline.execute("scrapy crawl toutiao --nolog".split())
+
+def job():
+    cmdline.execute("scrapy crawl toutiao --nolog".split())
+
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', seconds=60 * 30, next_run_time=datetime.datetime.now())
+def run():
+    process = multiprocessing.Process(target=job)
+    process.start()
+
+
+if __name__ == '__main__':
+    sched.start()
+# cmdline.execute("scrapy crawl toutiao --nolog".split())
